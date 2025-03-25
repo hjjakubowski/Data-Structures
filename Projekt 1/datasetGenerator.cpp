6979b33b4a79d41e1e064e17b9629580af04597e
Hubert Jakubowski <264836@student.pwr.edu.pl>
2025-03-25 15:36:09 +0100

    Fixed dataset generator

--- Projekt 1/datasetGenerator.cpp
+++ Projekt 1/datasetGenerator.cpp
@@ -7,33 +7,44 @@
 
 using namespace std;
 
-int main() {
-    cout << "Podaj iloœæ liczb do wygenerowania: (min 10 000 x " << pow(10, n) << " dla n>=0)" << endl;
-	cin >> n;
-    
-	string fileName = "dataset_" + to_string(n) + ".txt";
+/*
+void datasetGenerator() {
 
-	if (n < 10000) {
-		cout << "Podana liczba jest za ma³a!" << endl;
-		return 1;
-	}
+    string fileName = "dataset_" + to_string(n) + ".txt";
+
+
 
     ofstream outFile(fileName);
     if (!outFile) {
-        cerr << "Nie mo¿na otworzyæ pliku do zapisu!" << endl;
-        return 1;
+        cerr << "Nie mozna otworzyc pliku do zapisu!" << endl;
     }
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, n);
+    uniform_int_distribution<> dis(1, 4294967295);
 
     for (int i = 0; i < n; ++i) {
-        outFile << dis(gen) << endl;
+        outFile << dis(gen) << ",";
     }
 
     outFile.close();
     cout << "Dataset wygenerowany i zapisany do pliku dataset.txt" << endl;
 
-    return 0;
 }
+
+int main() {
+	datasetGenerator(1000);
+	datasetGenerator(5000);
+	datasetGenerator(10000);
+	datasetGenerator(50000);
+	datasetGenerator(100000);
+    datasetGenerator(250000);
+	datasetGenerator(500000);
+	datasetGenerator(750000);
+	datasetGenerator(1000000);
+	datasetGenerator(2500000);
+	datasetGenerator(5000000);
+	datasetGenerator(7500000);
+	datasetGenerator(10000000);
+}
+*/
