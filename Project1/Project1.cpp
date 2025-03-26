45b4c40298f2f741acfeb3feeb2e35e3ab6ec89b
Hubert Jakubowski <264836@student.pwr.edu.pl>
2025-03-26 13:25:23 +0100

    Fixing the cardinal error of including the doublyLinkedList.cpp instead of hpp a main program. Moving all the methond implementations to doublyLinkedList.hpp to avoid Linker errors realted with template usage during compilation. Last but not list updating the propper file that I acctually code on in vs project, instead of the egg-shells of files which where created in the beggining, to actually keep the functioning code

---
+++ Project1/Project1.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <string>
+#include "doublyLinkedList.hpp"
+
+
+using namespace std;
+using namespace std::chrono;
+
+
+
+void loadDatasetToList(const string& fileName, DLinkedList<int>& list) {
+    ifstream inFile(fileName);
+    if (!inFile) {
+        cerr << "Nie mozna otworzyc pliku do odczytu!" << endl;
+        return;
+    }
+
+    string line;
+    while (getline(inFile, line, ',')) {
+        stringstream ss(line);
+        int number;
+        if (ss >> number) {
+            list.addBack(number);
+        }
+    }
+
+    inFile.close();
+}
+
+
+void performTest(DLinkedList<int>& list, const string& operation, const string& datasetName, int element = 0, int position = 0) {
+    auto start = high_resolution_clock::now();
+    if (operation == "addFront") {
+        list.addFront(element);
+    }
+    else if (operation == "addBack") {
+        list.addBack(element);
+    }
+    else if (operation == "add") {
+        list.add(element, position);
+    }
+    else if (operation == "removeFront") {
+        list.removeFront();
+    }
+    else if (operation == "removeBack") {
+        list.removeBack();
+    }
+    else if (operation == "remove") {
+        list.remove(position);
+    }
+    else if (operation == "find") {
+        list.Find(element);
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(end - start);
+    cout << "Czas wykonania operacji " << operation << " na zbiorze " << datasetName << ": " << duration.count() << " ns" << endl;
+}
+
+int main() {
+    string datasets[] = {
+        "dataset_5000.txt", "dataset_10000.txt", "dataset_50000.txt", "dataset_100000.txt",
+        "dataset_200000.txt", "dataset_300000.txt", "dataset_400000.txt", "dataset_500000.txt",
+        "dataset_600000.txt", "dataset_700000.txt", "dataset_800000.txt","dataset_900000.txt", "dataset_1000000.txt"
+    };
+    while (true) {
+        cout << "Wybierz strukture danych do testowania:" << endl;
+        cout << "1. Tablica dynamiczna" << endl;
+        cout << "2. Lista jednokierunkowa" << endl;
+        cout << "3. Lista dwukierunkowa" << endl;
+        int structureChoice;
+        cin >> structureChoice;
+
+        cout << "Wybierz rodzaj testu:" << endl;
+        cout << "1. Dodanie elementu na początek" << endl;
+        cout << "2. Dodanie elementu na koniec" << endl;
+        cout << "3. Dodanie elementu w dowolnym miejscu" << endl;
+        cout << "4. Usunięcie początkowego elementu" << endl;
+        cout << "5. Usunięcie końcowego elementu" << endl;
+        cout << "6. Usunięcie elementu z dowolnej pozycji" << endl;
+        cout << "7. Znalezienie podanego przez użytkownika elementu" << endl;
+        int testChoice;
+        cin >> testChoice;
+
+        string operation;
+        int element = 0;
+        int position = 0;
+        switch (testChoice) {
+        case 1: operation = "addFront"; break;
+        case 2: operation = "addBack"; break;
+        case 3: operation = "add";
+            cout << "Podaj element do dodania: ";
+            cin >> element;
+            cout << "Podaj pozycje: ";
+            cin >> position;
+            break;
+        case 4: operation = "removeFront"; break;
+        case 5: operation = "removeBack"; break;
+        case 6: operation = "remove";
+            cout << "Podaj pozycje: ";
+            cin >> position;
+            break;
+        case 7: operation = "find";
+            cout << "Podaj element do znalezienia: ";
+            cin >> element;
+            break;
+        default: cout << "Nieprawidłowy wybór!" << endl; continue;
+        }
+        
+        for (int i = 0; i < 13; ++i) {
+            string datasetName = datasets[i];
+            DLinkedList<int> list;
+            loadDatasetToList(datasetName, list);
+            performTest(list, operation, datasetName, element, position);
+            //cout << list.getSize() << endl;
+        }
+        
+        cout << "Czy chcesz wykonac kolejne testy? (t/n): ";
+        char cont;
+        cin >> cont;
+        if (cont == 'n' || cont == 'N') {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+
+	
+   
+
---
+++ Project1/doublyLinkedList.hpp
@@ -0,0 +1,156 @@
+﻿#pragma once
+#ifndef DOUBLYLINKEDLIST_HPP
+#define DOUBLYLINKEDLIST_HPP
+
+#include <iostream>
+
+template <typename Elem>
+class DLinkedList {
+public:
+    DLinkedList();
+    ~DLinkedList();
+
+    bool empty() const;
+    int getSize();
+    void addFront(const Elem& e);
+    void addBack(const Elem& e);
+    void removeFront();
+    void removeBack();
+	void add(const Elem& e, int pos);
+	void remove(int pos);
+    int Find(const Elem& e);
+
+    
+    
+
+private:
+    struct DNode {
+        Elem elem;
+        DNode* next;
+        DNode* prev;
+    };
+
+    DNode* header;
+    DNode* trailer;
+};
+
+template <typename Elem>
+DLinkedList<Elem>::DLinkedList() {
+    header = new DNode;
+    trailer = new DNode;
+    header->next = trailer;
+    trailer->prev = header;
+}
+
+template <typename Elem>
+DLinkedList<Elem>::~DLinkedList() {
+    while (!empty()) removeFront();
+    delete header;
+    delete trailer;
+}
+
+template <typename Elem>
+bool DLinkedList<Elem>::empty() const {
+    return header->next == trailer;
+}
+
+template <typename Elem>
+int DLinkedList<Elem>::getSize() {
+    int size = 0;
+    DNode* temp = header->next;
+    while (temp != trailer) {
+        size++;
+        temp = temp->next;
+    }
+    return size;
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::addFront(const Elem& e) {
+    DNode* newNode = new DNode;
+    newNode->elem = e;
+    newNode->next = header->next;
+    newNode->prev = header;
+    header->next->prev = newNode;
+    header->next = newNode;
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::addBack(const Elem& e) {
+    DNode* newNode = new DNode;
+    newNode->elem = e;
+    newNode->prev = trailer->prev;
+    newNode->next = trailer;
+    trailer->prev->next = newNode;
+    trailer->prev = newNode;
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::removeFront() {
+    if (empty()) throw std::runtime_error("Empty list!");
+    if (!empty()) {
+        DNode* oldNode = header->next;
+        header->next = oldNode->next;
+        oldNode->next->prev = header;
+        delete oldNode;
+    }
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::removeBack() {
+    if (empty()) throw std::runtime_error("Empty list!");
+    if (!empty()) {
+        DNode* oldNode = trailer->prev;
+        trailer->prev = oldNode->prev;
+        oldNode->prev->next = trailer;
+        delete oldNode;
+    }
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::add(const Elem& e, int pos) {
+    if (pos < 0 || pos > getSize()) throw std::out_of_range("Invalid position!");
+
+    DNode* newNode = new DNode;
+    newNode->elem = e;
+
+    if (pos == 0) addFront(e);
+    else if (pos == getSize()) addBack(e);
+    else {
+        DNode* current = header->next;
+        for (int i = 0; i < pos - 1; i++) current = current->next;
+
+        newNode->next = current;
+        newNode->prev = current->prev;
+        current->prev->next = newNode;
+        current->prev = newNode;
+    }
+}
+
+template <typename Elem>
+void DLinkedList<Elem>::remove(int pos) {
+    if (pos < 0 || pos >= getSize())throw std::out_of_range("Invalid position");
+
+    DNode* current = header->next;
+    for (int i = 0; i < pos; ++i) current = current->next;
+
+
+    current->prev->next = current->next;
+    current->next->prev = current->prev;
+    delete current;
+}
+
+template <typename Elem>
+int DLinkedList<Elem>::Find(const Elem& e) {
+    DNode* current = header->next;
+    int index = 0;
+    while (current != trailer) {
+        if (current->elem == e) {
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+    return -1;
+}
+#endif
