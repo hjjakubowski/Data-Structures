50023ccfefa92e6fbc66b1667b88ec59b9eb720a
Ol3hhh <olehmaru@gmail.com>
2025-04-08 21:32:18 +0200

    comments added

--- Project1/SingleLinkList.hpp
+++ Project1/SingleLinkList.hpp
@@ -1,171 +1,183 @@
 #pragma once
 #include <iostream>
 
+// Struktura reprezentująca pojedynczy węzeł listy
 template <typename T>
 struct Node {
-    T item;
-    Node* next;
+    T item;        // Element przechowywany w węźle
+    Node* next;    // Wskaźnik na następny węzeł
 
-    Node(T item, Node* next = nullptr) : item(item), next(next) {}
+    Node(T item, Node* next = nullptr) : item(item), next(next) {} // Konstruktor węzła
 };
 
 template <typename T>
 class Singlelist {
 private:
-    int size;
-    Node<T>* head;
+    int size;       // Liczba elementów w liście
+    Node<T>* head;  // Wskaźnik na pierwszy element listy
 
 public:
-    Singlelist();
-    ~Singlelist();
+    Singlelist();   // Konstruktor listy
+    ~Singlelist();  // Destruktor listy
 
-    int getSize() const;
+    int getSize() const;  // Zwraca aktualny rozmiar listy
 
-    void addBack(T item);
-    void addFront(T item);
-    void add(T item, int index);
+    void addBack(T item);  // Dodaje element na koniec listy
+    void addFront(T item); // Dodaje element na początek listy
+    void add(T item, int index);  // Dodaje element w określonym indeksie
 
-    void removeBack();
-    void removeFront();
-    void remove(int index);
+    void removeBack();  // Usuwa ostatni element z listy
+    void removeFront(); // Usuwa pierwszy element z listy
+    void remove(int index);  // Usuwa element na podanym indeksie
 
-    int Find(T item) const;
-    T& operator[](const int index);
+    int Find(T item) const; // Zwraca indeks pierwszego wystąpienia elementu, lub -1 jeśli nie znaleziono
+    T& operator[](const int index);  // Operator [] do dostępu do elementów listy
 };
 
+// Konstruktor listy - inicjalizuje pustą listę
 template <typename T>
 Singlelist<T>::Singlelist() : size(0), head(nullptr) {}
 
+// Destruktor listy - zwalnia pamięć zajmowaną przez listę
 template <typename T>
 Singlelist<T>::~Singlelist() {
     while (head) {
-        removeFront();
+        removeFront();  // Usuwamy wszystkie elementy z listy
     }
 }
 
+// Zwraca rozmiar listy
 template <typename T>
 int Singlelist<T>::getSize() const {
     return size;
 }
 
+// Dodaje element na koniec listy
 template <typename T>
 void Singlelist<T>::addBack(T item) {
-    Node<T>* newNode = new Node<T>(item);
-    if (!head) {
+    Node<T>* newNode = new Node<T>(item); // Tworzymy nowy węzeł
+    if (!head) { // Jeśli lista jest pusta, ustawiamy nowy węzeł jako pierwszy
         head = newNode;
     }
     else {
         Node<T>* current = head;
-        while (current->next) {
+        while (current->next) { // Przechodzimy do ostatniego elementu listy
             current = current->next;
         }
-        current->next = newNode;
+        current->next = newNode;  // Dodajemy nowy węzeł na końcu listy
     }
-    size++;
+    size++; 
 }
 
+// Usuwa ostatni element listy
 template <typename T>
 void Singlelist<T>::removeBack() {
-    if (!head) return;
+    if (!head) return; // Jeśli lista jest pusta, nic nie robimy
 
     Node<T>* current = head;
-    if (!current->next) {
+    if (!current->next) {  // Jeśli lista ma tylko jeden element
         delete head;
-        head = nullptr;
+        head = nullptr;  // Usuwamy jedyny element i ustawiamy wskaźnik na null
     }
     else {
-        while (current->next && current->next->next) {
+        while (current->next && current->next->next) { // Przechodzimy do przedostatniego elementu
             current = current->next;
         }
-        delete current->next;
-        current->next = nullptr;
+        delete current->next; // Usuwamy ostatni element
+        current->next = nullptr;  // Ustawiamy wskaźnik na null, by zakończyć listę
     }
-    size--;
+    size--;  
 }
 
+// Dodaje element na początek listy
 template <typename T>
 void Singlelist<T>::addFront(T item) {
-    head = new Node<T>(item, head);
-    size++;
+    head = new Node<T>(item, head); // Tworzymy nowy węzeł, który wskazuje na obecny pierwszy element
+    size++; 
 }
 
+// Usuwa pierwszy element listy
 template <typename T>
 void Singlelist<T>::removeFront() {
     if (head) {
         Node<T>* temp = head;
-        head = head->next;
-        delete temp;
-        size--;
+        head = head->next;  // Ustawiamy wskaźnik na drugi element
+        delete temp;  // Usuwamy pierwszy element
+        size--;  
     }
 }
 
+// Dodaje element w określonym indeksie
 template <typename T>
 void Singlelist<T>::add(T item, int index) {
-    if (index < 0 || index > size) {
+    if (index < 0 || index > size) { // Sprawdzamy, czy indeks jest w odpowiednim zakresie
         std::cout << "Index out of range." << std::endl;
         return;
     }
 
-    if (index == 0) {
+    if (index == 0) {  // Jeśli indeks to 0, dodajemy element na początek
         addFront(item);
     }
     else {
         Node<T>* previous = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index - 1; i++) { // Przechodzimy do elementu przed wskazywanym indeksem
             previous = previous->next;
         }
-        Node<T>* newNode = new Node<T>(item, previous->next);
-        previous->next = newNode;
-        size++;
+        Node<T>* newNode = new Node<T>(item, previous->next); // Tworzymy nowy węzeł
+        previous->next = newNode;  // Łączymy go z poprzednim elementem
+        size++; 
     }
 }
 
+// Usuwa element o podanym indeksie
 template <typename T>
 void Singlelist<T>::remove(int index) {
-    if (index < 0 || index >= size) {
+    if (index < 0 || index >= size) { // Sprawdzamy, czy indeks jest w odpowiednim zakresie
         std::cout << "Enter proper index" << std::endl;
         return;
     }
 
-    if (index == 0) {
+    if (index == 0) {  // Jeśli indeks to 0, usuwamy pierwszy element
         removeFront();
     }
     else {
         Node<T>* previous = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index - 1; i++) { // Przechodzimy do elementu przed wskazywanym indeksem
             previous = previous->next;
         }
-        Node<T>* temp = previous->next;
-        previous->next = temp->next;
-        delete temp;
-        size--;
+        Node<T>* temp = previous->next;  // Wskaźnik na element do usunięcia
+        previous->next = temp->next;  // Łączymy poprzedni element z następnym
+        delete temp;  // Usuwamy element
+        size--;  
     }
 }
 
+// Zwraca indeks pierwszego wystąpienia danego elementu w liście, lub -1 jeśli nie znaleziono
 template <typename T>
 int Singlelist<T>::Find(T item) const {
     Node<T>* current = head;
     int index = 0;
     while (current) {
-        if (current->item == item) {
+        if (current->item == item) {  // Jeśli element jest równy szukanemu, zwracamy indeks
             return index;
         }
-        current = current->next;
+        current = current->next;  // Przechodzimy do następnego elementu
         index++;
     }
-    return -1;
+    return -1;  // Zwracamy -1, jeśli element nie został znaleziony
 }
 
+// Operator [] do dostępu do elementów listy
 template <typename T>
 T& Singlelist<T>::operator[](const int index) {
-    if (index < 0 || index >= size) {
+    if (index < 0 || index >= size) { // Sprawdzamy, czy indeks jest w odpowiednim zakresie
         std::cout << "Index out of bounds" << std::endl;
-        throw std::out_of_range("Index out of bounds");
+        throw std::out_of_range("Index out of bounds"); // Rzucamy wyjątek, jeśli indeks jest niepoprawny
     }
 
     Node<T>* current = head;
-    for (int i = 0; i < index; i++) {
+    for (int i = 0; i < index; i++) {  // Przechodzimy do wskazywanego elementu
         current = current->next;
     }
-    return current->item;
+    return current->item;  // Zwracamy element w wskazanym indeksie
 }
--- Project1/arrayList.hpp
+++ Project1/arrayList.hpp
@@ -1,147 +1,154 @@
 #pragma once
 #include <iostream>
-#include <stdexcept>
 
 template <typename T>
 class Arraylist {
 private:
-    T* arr;
-    int size;
-    int capacity;
+    T* arr;           // Wskaźnik na dynamicznie alokowaną tablicę
+    int size;         
+    int capacity;     
 
-    void increaseSize();
+    void increaseSize(); // Funkcja zwiększająca rozmiar tablicy
 
 public:
-    Arraylist(int capacity = 2);
-    ~Arraylist();
+    Arraylist(int capacity = 2);  // początkowy rozmiar tablicy - 2
+    ~Arraylist();                 
 
-    int getSize() const;
-    int getCapacity() const;
+    int getSize() const;         
+    int getCapacity() const;    
 
-    void addBack(T item);
-    void addFront(T item);
-    void add(T item, int index);
-    void removeBack();
-    void removeFront();
-    void remove(int index);
+    void addBack(T item);       
+    void addFront(T item);       
+    void add(T item, int index); 
+    void removeBack();          
+    void removeFront();         
+    void remove(int index);     
 
-    int Find(T item) const;
+    int Find(T item) const;     
 
-    T& operator[](const int index);
+    T& operator[](const int index); 
 };
 
-
-
-
-
-
+// Implementacja konstruktora
 template <typename T>
 Arraylist<T>::Arraylist(int capacity) : size(0), capacity(capacity) {
-    arr = new T[capacity];
+    arr = new T[capacity]; // Alokacja pamięci na tablicę o początkowej pojemności
 }
 
+// Destruktor - zwalnia pamięć zajmowaną przez tablicę
 template <typename T>
 Arraylist<T>::~Arraylist() {
-    delete[] arr;
+    delete[] arr; // Zwalnia pamięć dynamiczną
 }
 
+// Zwiększa pojemność tablicy dwukrotnie, jeśli brakuje miejsca
 template <typename T>
 void Arraylist<T>::increaseSize() {
-    if (size == capacity) {
-        capacity *= 2;
-        T* temp = new T[capacity];
+    if (size == capacity) { // Jeśli tablica jest pełna
+        capacity *= 2; // Podwajamy pojemność
+        T* temp = new T[capacity]; // Tworzymy nową tablicę o większej pojemności
 
         for (int i = 0; i < size; i++) {
-            temp[i] = arr[i];
+            temp[i] = arr[i]; // Kopiujemy elementy do nowej tablicy
         }
 
-        delete[] arr;
-        arr = temp;
+        delete[] arr; // Zwalniamy starą tablicę
+        arr = temp;    // Aktualizujemy wskaźnik do nowej tablicy
     }
 }
 
+// Zwraca aktualny rozmiar tablicy
 template <typename T>
 int Arraylist<T>::getSize() const {
     return size;
 }
 
+// Zwraca aktualną pojemność tablicy
 template <typename T>
 int Arraylist<T>::getCapacity() const {
     return capacity;
 }
 
+// Dodaje element na koniec tablicy
 template <typename T>
 void Arraylist<T>::addBack(T item) {
-    increaseSize();
-    arr[size] = item;
-    size++;
+    increaseSize(); // Sprawdzamy, czy trzeba zwiększyć pojemność
+    arr[size] = item; // Dodajemy element na koniec
+    size++; 
 }
 
+// Dodaje element na początek tablicy
 template <typename T>
 void Arraylist<T>::addFront(T item) {
-    increaseSize();
+    increaseSize(); // Sprawdzamy, czy trzeba zwiększyć pojemność
     for (int i = size; i > 0; i--) {
-        arr[i] = arr[i - 1];
+        arr[i] = arr[i - 1]; // Przesuwamy elementy o jedno miejsce w prawo
     }
-    arr[0] = item;
-    size++;
+    arr[0] = item; // Wstawiamy element na początek
+    size++; 
 }
 
+// Dodaje element w określonym indeksie
 template <typename T>
 void Arraylist<T>::add(T item, int index) {
-    if (index < 0 || index > size) return; 
-    increaseSize();
+    if (index < 0 || index > size) return; // Sprawdzamy, czy indeks jest prawidłowy
+    increaseSize(); // Sprawdzamy, czy trzeba zwiększyć pojemność
     for (int i = size; i > index; i--) {
-        arr[i] = arr[i - 1];
+        arr[i] = arr[i - 1]; // Przesuwamy elementy w prawo
     }
-    arr[index] = item;
-    size++;
+    arr[index] = item; // Wstawiamy element w podanym indeksie
+    size++; 
 }
 
+// Usuwa ostatni element z tablicy
 template <typename T>
 void Arraylist<T>::removeBack() {
     if (size > 0) {
-        size--;
+        delete arr[size - 1];  // Zwolnienie pamięci, na którą wskazuje ostatni element
+        size--; 
     }
 }
 
+
+// Usuwa pierwszy element z tablicy
 template <typename T>
 void Arraylist<T>::removeFront() {
     if (size > 0) {
         for (int i = 1; i < size; i++) {
-            arr[i - 1] = arr[i];
+            arr[i - 1] = arr[i]; // Przesuwamy elementy w lewo
         }
-        size--;
+        size--; 
     }
 }
 
+// Usuwa element o podanym indeksie
 template <typename T>
 void Arraylist<T>::remove(int index) {
-    if (index < 0 || index >= size) return; 
+    if (index < 0 || index >= size) return; // Sprawdzamy, czy indeks jest prawidłowy
     if (size > 0) {
         for (int i = index; i < size - 1; i++) {
-            arr[i] = arr[i + 1];
+            arr[i] = arr[i + 1]; // Przesuwamy elementy w lewo
         }
-        size--;
+        size--; 
     }
 }
 
+// Zwraca indeks pierwszego wystąpienia danego elementu, lub -1 jeśli nie znaleziono
 template <typename T>
 int Arraylist<T>::Find(T item) const {
     for (int i = 0; i < size; i++) {
         if (arr[i] == item) {
-            return i;
+            return i; // Zwracamy indeks pierwszego wystąpienia
         }
     }
-    return -1;
+    return -1; // Zwracamy -1, jeśli element nie został znaleziony
 }
 
+// Operator dostępu do elementu - jak dla zwykłej tablicy
 template <typename T>
 T& Arraylist<T>::operator[](const int index) {
     if (index < 0 || index >= size) {
-        std::cout << "Index out of bounds" << std::endl;
-        throw std::out_of_range("Index out of bounds");
+        std::cout << "Index out of bounds" << std::endl; // Jeśli indeks jest poza zakresem
     }
-    return arr[index];
+    return arr[index]; // Zwracamy element w podanym indeksie
 }
-
