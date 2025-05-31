f994e756d6fa4a855d6ffbdb3d850d362d57cd5e
Hubert Jakubowski <264836@student.pwr.edu.pl>
2025-05-31 17:06:11 +0200

    Kinda skipped few commits while implementing this one. Made it with templates from the very begging, solved few linker problems by adding 2 constructors.

---
+++ Project3/linearProbingHM.hpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <stdexcept>
+#include <limits>
+
+template <typename KeyType, typename ValueType>
+class HashMapLinearProbing {
+    struct HashNode {
+        ValueType value;
+        KeyType key;
+        HashNode(KeyType key, ValueType value)
+            : value(value), key(key) {
+        }
+    };
+
+    HashNode** arr;
+    int capacity;
+    int size;
+    HashNode* deletedNodeMarker;
+    int hashCode(const KeyType& key) const;
+
+public:
+	HashMapLinearProbing();
+    HashMapLinearProbing(int initialCapacity);
+    ~HashMapLinearProbing();
+
+    void insert(const KeyType& key, const ValueType& value);
+    ValueType remove(const KeyType& key);
+    void print() const;
+};
+
+template <typename KeyType, typename ValueType>
+HashMapLinearProbing<KeyType, ValueType>::HashMapLinearProbing()
+    : HashMapLinearProbing(20) {}
+
+template <typename KeyType, typename ValueType>
+HashMapLinearProbing<KeyType, ValueType>::HashMapLinearProbing(int initialCapacity)
+    : capacity(initialCapacity), size(0)
+{
+    arr = new HashNode * [capacity];
+    for (int i = 0; i < capacity; i++)
+        arr[i] = nullptr;
+
+    deletedNodeMarker = new HashNode(KeyType(-1), ValueType());
+}
+
+template <typename KeyType, typename ValueType>
+HashMapLinearProbing<KeyType, ValueType>::~HashMapLinearProbing()
+{
+    for (int i = 0; i < capacity; ++i) {
+        if (arr[i] && arr[i] != deletedNodeMarker)
+            delete arr[i];
+    }
+    delete[] arr;
+    delete deletedNodeMarker;
+}
+
+template <typename KeyType, typename ValueType>
+int HashMapLinearProbing<KeyType, ValueType>::hashCode(const KeyType& key) const
+{
+    return static_cast<int>(key) % capacity; 
+}
+
+template <typename KeyType, typename ValueType>
+void HashMapLinearProbing<KeyType, ValueType>::insert(const KeyType& key, const ValueType& value)
+{
+    if (size >= capacity * 0.7) { 
+
+        HashNode** oldArr = arr;
+        int oldCapacity = capacity;
+
+        capacity *= 2;
+        arr = new HashNode * [capacity];
+        for (int i = 0; i < capacity; ++i) arr[i] = nullptr;
+
+        for (int i = 0; i < oldCapacity; ++i) {
+            if (oldArr[i] && oldArr[i] != deletedNodeMarker) {
+                int newIndex = hashCode(oldArr[i]->key);
+                while (arr[newIndex] != nullptr) {
+                    newIndex = (newIndex + 1) % capacity;
+                }
+                arr[newIndex] = oldArr[i];
+            }
+        }
+        delete[] oldArr;
+    }
+    int hashIndex = hashCode(key);
+    int startIndex = hashIndex;
+    do {
+        if (arr[hashIndex] == nullptr || arr[hashIndex] == deletedNodeMarker) {
+            arr[hashIndex] = new HashNode(key, value);
+            ++size;
+            return;
+        }
+        else if (arr[hashIndex]->key == key) {
+            arr[hashIndex]->value = value;
+            return;
+        }
+        hashIndex = (hashIndex + 1) % capacity;
+    } while (hashIndex != startIndex);
+
+    throw std::overflow_error("Hash table overflow (no empty slot)");
+
+}
+
+template <typename KeyType, typename ValueType>
+ValueType HashMapLinearProbing<KeyType, ValueType>::remove(const KeyType& key)
+{
+    int hashIndex = hashCode(key);
+    int startIndex = hashIndex;
+    do {
+        if (arr[hashIndex] == nullptr) {
+            throw std::out_of_range("Key not found");
+        }
+        if (arr[hashIndex] != deletedNodeMarker && arr[hashIndex]->key == key) {
+            ValueType val = arr[hashIndex]->value;
+            delete arr[hashIndex];
+            arr[hashIndex] = deletedNodeMarker;
+            --size;
+            return val;
+        }
+        hashIndex = (hashIndex + 1) % capacity;
+    } while (hashIndex != startIndex);
+
+    throw std::out_of_range("Key not found");
+}
+
+template <typename KeyType, typename ValueType>
+void HashMapLinearProbing<KeyType, ValueType>::print() const
+{
+    for (int i = 0; i < capacity; i++) {
+        if (arr[i] && arr[i] != deletedNodeMarker)
+            std::cout << "key = " << arr[i]->key << "  value = " << arr[i]->value << std::endl;
+    }
+}
